187a1cd1b7d044074402ac31374174235a8fc8d9
Xacobo Freire <xacobo@ticgal.com>
2026-01-26 16:48:22 +0100

    new beta

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_BORGBASE_VERSION', '1.1.6');
+define('PLUGIN_BORGBASE_VERSION', '2.0.0-beta');
 define("PLUGIN_BORGBASE_MIN_GLPI_VERSION", "11.0");
 define("PLUGIN_BORGBASE_MAX_GLPI_VERSION", "12.0");
 define("PLUGIN_BORGBASE_ICON", "fa-solid fa-hard-drive");
